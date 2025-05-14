67ab2c53e6d893c0ec41d5839a59fc8f5a763cb7
Melissa Solórzano <163376726+kmmelissat@users.noreply.github.com>
2025-05-13 19:38:59 -0600

    Import EventModule in app.module.ts to enhance application functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VideogamesModule } from './videogames/videogames.module';
+import { EventModule } from './event/event.module';
 
 @Module({
-  imports: [VideogamesModule],
+  imports: [VideogamesModule, EventModule],
   controllers: [AppController],
   providers: [AppService],
 })
