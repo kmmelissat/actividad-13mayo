0fde93cba6e367756d06004f1dea860eb235e179
Melissa Solórzano <163376726+kmmelissat@users.noreply.github.com>
2025-05-16 15:21:03 -0600

    Refactor app.module.ts to include OrdersModule and clean up controller and provider definitions; enhance global validation pipe configuration in main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,21 @@
 import { Module } from '@nestjs/common';
-import { APP_PIPE } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { OrdersController } from './orders/orders.controller';
-import { OrdersService } from './orders/orders.service';
 import { VideogamesModule } from './videogames/videogames.module';
 import { EventModule } from './event/event.module';
 import { BillsModule } from './bills/bills.module';
 import { BooksModule } from './books/books.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [VideogamesModule, EventModule, BillsModule, BooksModule],
-  controllers: [AppController,OrdersController],
-  providers: [
-    AppService,
-    OrdersService,
-    {
-      provide: APP_PIPE,
-      useValue: new ValidationPipe({
-        whitelist: true,
-        transform: true,
-        forbidNonWhitelisted: true,
-      }),
-    },
+  imports: [
+    VideogamesModule,
+    EventModule,
+    BillsModule,
+    BooksModule,
+    OrdersModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { OrdersModule } from './orders/orders.module';
 import { ValidationPipe } from '@nestjs/common';
 
 const PORT = 8000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(PORT);
   console.log('🚨🚨🚨🚨');
   console.log(`Server is running on port ${PORT}`);
