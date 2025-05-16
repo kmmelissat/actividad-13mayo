5bca8b27b4cc198c53e6f8bfc88595366f5c4a28
Melissa Solórzano <163376726+kmmelissat@users.noreply.github.com>
2025-05-15 23:16:04 -0600

    Refactor VideogamesService and CreateVideogameDto to improve videogame creation logic and add genre validation

--- src/videogames/dto/create-videogame.dto.ts
+++ src/videogames/dto/create-videogame.dto.ts
@@ -1,7 +1,11 @@
 import { IsString, MinLength } from 'class-validator';
 
 export class CreateVideogameDto {
-    @IsString()
-    @MinLength(5, { message: 'Videogame name must be longer than 4 characters' })
-    name: string;
-} 
+  @IsString()
+  @MinLength(5, { message: 'Videogame name must be longer than 4 characters' })
+  name: string;
+
+  @IsString()
+  @MinLength(3, { message: 'Genre must be longer than 2 characters' })
+  genre: string;
+}
--- src/videogames/videogames.service.ts
+++ src/videogames/videogames.service.ts
@@ -3,39 +3,39 @@ import { CreateVideogameDto } from './dto/create-videogame.dto';
 
 @Injectable()
 export class VideogamesService {
-    private videogames = [
-        {
-        id: 1,
-        name: 'Minecraft',
-        genre: 'Sandbox',
-        date: new Date('2011-11-18')
+  private videogames = [
+    {
+      id: 1,
+      name: 'Minecraft',
+      genre: 'Sandbox',
+      date: new Date('2011-11-18'),
     },
     {
-        id: 2,
-        name: 'Grand Theft Auto V',
-        genre: 'Open World',
-        date: new Date('2013-09-17')
+      id: 2,
+      name: 'Grand Theft Auto V',
+      genre: 'Open World',
+      date: new Date('2013-09-17'),
     },
     {
-        id: 3,
-        name: 'Fortnite',
-        genre: 'Battle Royale',
-        date: new Date('2017-07-25')
-    }   
-];
+      id: 3,
+      name: 'Fortnite',
+      genre: 'Battle Royale',
+      date: new Date('2017-07-25'),
+    },
+  ];
 
-    findAll() {
-        return this.videogames;
-    }
+  findAll() {
+    return this.videogames;
+  }
 
-    create(createVideogameDto: CreateVideogameDto) {
-        const newVideogame = {
-            id: Date.now(),
-            name: createVideogameDto.name,
-            genre: 'Unknown',
-            date: new Date()
-        }
-        this.videogames.push(newVideogame)
-        return newVideogame
-    }
+  create(createVideogameDto: CreateVideogameDto) {
+    const newVideogame = {
+      id: this.videogames.length + 1,
+      name: createVideogameDto.name,
+      genre: createVideogameDto.genre,
+      date: new Date(),
+    };
+    this.videogames.push(newVideogame);
+    return newVideogame;
+  }
 }
