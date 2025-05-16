5e3fe785c7acb3b8360bba6d2440c4df8b8aab69
diegocabezas004 <diegocabezas72@gmail.com>
2025-05-16 15:03:48 -0600

    Orders hecho?
    🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { OrdersModule } from './orders/orders.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(OrdersModule);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
